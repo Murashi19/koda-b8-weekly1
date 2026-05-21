038212be3d28647c8318464b5d16d76d85c89f21
MuhamadRafliShidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-21 16:13:48 +0700

    chore: add mock data for development

---
+++ src/config/dataMenu.js
@@ -0,0 +1,62 @@
+// menu.js
+export const daftarMenu = {
+    friedChicken: [
+        { id: 1, nama: "Ayam Dada", harga: 18000 },
+        { id: 2, nama: "Ayam Paha Atas", harga: 18000 },
+        { id: 3, nama: "Ayam Paha Bawah", harga: 14000 },
+        { id: 4, nama: "Ayam Sayap", harga: 23000 }
+    ],
+    geprek: [
+        { id: 1, nama: "Ayam Geprek Dada", harga: 22000 },
+        { id: 2, nama: "Ayam Geprek Paha Atas", harga: 22000 },
+        { id: 3, nama: "Ayam Geprek Paha Bawah", harga: 19500 },
+        { id: 4, nama: "Ayam Geprek Sayap", harga: 18000 }
+    ],
+    sadazz: [
+        { id: 1, nama: "Sadazz Dada", harga: 23000 },
+        { id: 2, nama: "Sadazz Paha Atas", harga: 23000 },
+        { id: 3, nama: "Sadazz Paha Bawah", harga: 23000 },
+        { id: 4, nama: "Sadazz Sayap", harga: 23000 }
+    ],
+    ayamCLBK: [
+        { id: 1, nama: "Ayam CLBK Dada", harga: 22000 },
+        { id: 2, nama: "Ayam CLBK Paha Atas", harga: 22000 },
+        { id: 3, nama: "Ayam CLBK Paha Bawah", harga: 22000 },
+        { id: 4, nama: "Ayam CLBK Sayap", harga: 22000 }
+    ],
+    paketChicken: [
+        { id: 1, nama: "Paket Dada", harga: 22000 },
+        { id: 2, nama: "Paket Paha Atas", harga: 22000 },
+        { id: 3, nama: "Paket Paha Bawah", harga: 19500 },
+        { id: 4, nama: "Paket Sayap", harga: 18000 }
+    ],
+    paketGeprek: [
+        { id: 1, nama: "Paket Geprek Dada", harga: 22000 },
+        { id: 2, nama: "Paket Geprek Paha Atas", harga: 22000 },
+        { id: 3, nama: "Paket Geprek Paha Bawah", harga: 19500 },
+        { id: 4, nama: "Paket Geprek Sayap", harga: 18000 }
+    ],
+    sideDish: [
+        { id: 1, nama: "Nasi Putih", harga: 7000 },
+        { id: 2, nama: "Kentang", harga: 5000 },
+        { id: 3, nama: "Spaghetti", harga: 5000 }
+    ],
+    minuman: [
+        { id: 1, nama: "Air Mineral", harga: 8000 },
+        { id: 2, nama: "S-Tee", harga: 7000 },
+        { id: 3, nama: "Teh Botol Kotak Sosro", harga: 7500 }
+    ]
+};
+
+export const kategori = [
+    { id: 1, nama: "Fried Chicken" },
+    { id: 2, nama: "Geprek" },
+    { id: 3, nama: "Sadazz" },
+    { id: 4, nama: "Ayam CLBK" },
+    { id: 5, nama: "Paket Chicken" },
+    { id: 6, nama: "Paket Geprek" },
+    { id: 7, nama: "Side Dish" },
+    { id: 8, nama: "Minuman" }
+];
+
+
