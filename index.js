205593a1f3ebf5c8b0c3c1478e4c67a646527a18
MuhamadRafliShidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-21 15:22:27 +0700

    refactor: fix import syntax and remove unused variables

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import daftarMenu, { kategori } from './src/config/dataMenu.js';
-import pesanMenu from './src/utils/pesanMenu.js';
-import { input, closeInput } from './src/utils/readline.js';
+import { daftarMenu, kategori } from './src/config/dataMenu.js';
+import { handleFoodOrder } from './src/utils/pesanMenu.js';
+import { input } from './src/utils/inputData.js';
 
 // Function Menampilkan Menu
-async function main() {
+export async function main() {
 
     console.log("\n=================================");
     console.log("        MENU RESTO LAZATTO");
@@ -44,44 +44,44 @@ async function main() {
     switch (selectedMenu) {
 
         case 1:
-            pesanMenu(friedChicken, kategori[0].nama);
+            handleFoodOrder(friedChicken, kategori[0].nama);
             break;
 
         case 2:
-            pesanMenu(geprek, kategori[1].nama);
+            handleFoodOrder(geprek, kategori[1].nama);
             break;
 
         case 3:
-            pesanMenu(sadazz, kategori[2].nama);
+            handleFoodOrder(sadazz, kategori[2].nama);
             break;
 
         case 4:
-            pesanMenu(ayamCLBK, kategori[3].nama);
+            handleFoodOrder(ayamCLBK, kategori[3].nama);
             break;
 
         case 5:
-            pesanMenu(paketChicken, kategori[4].nama);
+            handleFoodOrder(paketChicken, kategori[4].nama);
             break;
 
         case 6:
-            pesanMenu(paketGeprek, kategori[5].nama);
+            handleFoodOrder(paketGeprek, kategori[5].nama);
             break;
 
         case 7:
-            pesanMenu(sideDish, kategori[6].nama);
+            handleFoodOrder(sideDish, kategori[6].nama);
             break;
 
         case 8:
-            pesanMenu(minuman, kategori[7].nama);
+            handleFoodOrder(minuman, kategori[7].nama);
             break;
 
         default:
             console.log("\nMenu tidak tersedia!");
             return main();
     }
-
-    closeInput();
 }
 
 // Jalankan Program
 main();
+
+export default main;
