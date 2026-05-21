a4b41c45cea360e262f1c96ec062f299bcdf1bd5
MuhamadRafliShidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-21 16:22:21 +0700

    feat : added repeat order feature to be able to order again from the menu

---
+++ src/utils/repeatOrder.js
@@ -0,0 +1,22 @@
+import { input } from "./inputData.js";
+import { main } from "../../index.js";
+import { showReceipt } from "./pembayaran.js";
+
+export async function handleRepeatOrder(orderList, totalPrice) {
+
+    const userInput = await input("\nApakah ingin pesan lagi? (y/t): ");
+    const userAnswer = userInput.toLowerCase();
+
+    // Pesan lagi
+    if (userAnswer === "y" || userAnswer === "ya") {
+        return main();
+    }
+    // Checkout
+    if (userAnswer === "t" || userAnswer === "tidak") {
+        return showReceipt(orderList, totalPrice);
+    }
+    console.log("\nJawaban tidak valid!");
+    return handleRepeatOrder(orderList, totalPrice);
+
+}
+export default handleRepeatOrder;
