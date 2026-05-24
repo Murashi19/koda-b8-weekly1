33d534c23f7d1a20559cd8a83c79269203ff049e
MuhamadRafliShidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-24 21:47:32 +0700

    feat: added validation features for other inputs such as food orders, purchase amounts, and purchase receipts

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,47 +1,62 @@
-/* eslint-disable no-console */
 import { kategori } from "../config/dataMenu.js";
-import { handleFoodOrder } from "./pesanMenu.js";
-import { main } from "../../index.js";
 
-
+// Validasi Input Menu
 export function validateInputMenu(inputMenu) {
+    // Validasi jika input bukan angka
     if (isNaN(inputMenu)) {
-        console.log("Input harus berupa angka!");
-        return setTimeout(main, 3000);
+        throw new Error("Input harus berupa angka!");
     }
+    // Validasi jika input kosong
     if (inputMenu === 0) {
-        console.log("Input tidak boleh kosong!");
-        return setTimeout(main, 3000);
+        throw new Error("Input tidak boleh kosong!");
     }
-    if (inputMenu < 1 || inputMenu > kategori.length) {
-        console.log("Menu tidak tersedia!");
-        return setTimeout(main, 3000);
+    // Validasi jika input tidak valid / lebih dari jumlah kategori
+    if (inputMenu < 0 || inputMenu > kategori.length) {
+        throw new Error("Menu tidak tersedia!");
+    }
+}
+
+export function validateReceipt(value) {
+    if (!value || value.length === 0) {
+        throw new Error("Belum ada pesanan.");
+    }
+}
+
+export function validatePayment(inputPayment) {
+    if (inputPayment == 0) {
+        throw new Error("\nInput pembayaran tidak boleh kosong!");
+    }
+    if (isNaN(inputPayment) || inputPayment.trim() === "") {
+        throw new Error("\nInput harus berupa angka yang valid!");
+    }
+    if (inputPayment < 0) {
+        throw new Error("\nJumlah pembayaran tidak boleh minus/negatif!");
     }
 }
 
 export function validateInputOrder(inputOrder) {
-    if (inputOrder === 0) {
-        console.log("Input tidak boleh kosong!");
+    if (inputOrder == 0) {
+        throw new Error("Input id makanan tidak boleh kosong!");
     }
     if (isNaN(inputOrder) || inputOrder.trim() === "") {
-        console.log("Input harus berupa angka!");
-        return handleFoodOrder();
+        throw new Error("Input harus berupa angka!");
     }
-    if (inputOrder < 1) {
-        console.log("Jumlah tidak valid!");
+    if (inputOrder < 0) {
+        throw new Error("ID makanan tidak valid!");
     }
 }
 
 export function validateInputQty(inputQty) {
-    if (inputQty === null) {
-        console.log("Input tidak boleh kosong!");
+    if (inputQty == 0) {
+        throw new Error("Input jumlah tidak boleh kosong!");
     }
     if (isNaN(inputQty) || inputQty.trim() === "") {
-        console.log("Input harus berupa angka!");
-        return handleFoodOrder();
+        throw new Error("Input harus berupa angka!");
     }
-    if (inputQty < 1) {
-        console.log("Jumlah tidak valid!");
-        return handleFoodOrder();
+    if (inputQty < 0) {
+        throw new Error("Jumlah tidak boleh minus!");
+    }
+    if (inputQty > 100) {
+        throw new Error("Jumlah terlalu banyak!");
     }
 };
